8cff6a3677d56368e83370b4d78b5bbd779ab6a6
Watyson <watysonsoares@hotmail.com>
2025-05-05 17:03:38 -0300

    Ajustes no banco de dados

--- src/main/java/org/project/domain/entity/Produto.java
+++ src/main/java/org/project/domain/entity/Produto.java
@@ -1,17 +1,36 @@
 package org.project.domain.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 import java.math.BigDecimal;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
-@ToString
+@Data
+@Entity
+@Table(name = "produto")
 public class Produto {
-    private String id;
+    @Id
+    @Column(nullable = false, updatable = false, unique = true)
+    private Integer id;
+
+    @Column(nullable = false)
+    @NotBlank(message = "{campo.obrigatorio}")
     private String nome;
+
+    @Column(nullable = false)
+    @NotBlank(message = "{campo.obrigatorio}")
     private String descricao;
+
+    @Column(nullable = false, precision = 16, scale = 4)
+    @NotNull(message = "{campo.obrigatorio}")
+    @Positive(message = "{campo.positivo}")
     private BigDecimal preco;
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+CREATE TABLE produto (
+    id VARCHAR(255) NOT NULL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    descricao VARCHAR(255),
+    preco numeric(16, 4)
+);
