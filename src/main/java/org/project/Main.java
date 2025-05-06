2cad3644568be6732d3ab1caae0010f5b13f25ae
Watyson <watysonsoares@hotmail.com>
2025-05-06 01:08:07 -0300

    Feature: Retirando o banner da inicialização do Spring

--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -1,11 +1,14 @@
 package org.project;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(Main.class);
+        builder.bannerMode(Banner.Mode.OFF);
+        builder.run(args);
     }
 }
